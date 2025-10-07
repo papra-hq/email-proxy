03108ea59c7254461fe3403e8c866c4a46998538
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-10-07 14:19:50 +0200

    feat(email): enhance parseEmail function to include realTo and realFrom parameters

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
+import type { Address, Email } from 'postal-mime';
 import type { Env } from './types';
 import { triggerWebhook } from '@owlrelay/webhook';
 import PostalMime from 'postal-mime';
 
-async function parseEmail({ rawMessage }: { rawMessage: ReadableStream<Uint8Array> }) {
-  const emailBuffer = await new Response(rawMessage).arrayBuffer();
-  const email = await new PostalMime().parse(emailBuffer);
+async function parseEmail({
+  rawMessage,
+  realTo,
+  realFrom,
+}: {
+  rawMessage: ReadableStream<Uint8Array>;
+  realTo: string;
+  realFrom: string;
+}): Promise<{ email: Email & { originalTo: Address[]; originalFrom: Address } }> {
+  const rawEmail = new Response(rawMessage);
+  const parser = new PostalMime();
 
-  return { email };
+  const emailBuffer = await rawEmail.arrayBuffer();
+  const email = await parser.parse(emailBuffer);
+
+  return {
+    email: {
+      ...email,
+      originalTo: email.to ?? [],
+      originalFrom: email.from,
+      to: [
+        {
+          address: realTo,
+          name: email.to?.find(to => to.address === realTo)?.name ?? '',
+        },
+      ],
+      from: {
+        address: realFrom,
+        name: email.from.address === realFrom ? email.from.name : '',
+      },
+    },
+  };
 }
 
 function parseConfig({ env }: { env: Env }) {
@@ -26,7 +54,11 @@ function parseConfig({ env }: { env: Env }) {
 export default {
   async email(message: ForwardableEmailMessage, env: Env): Promise<void> {
     const { webhookUrl, webhookSecret } = parseConfig({ env });
-    const { email } = await parseEmail({ rawMessage: message.raw });
+    const { email } = await parseEmail({
+      rawMessage: message.raw,
+      realTo: message.to,
+      realFrom: message.from,
+    });
 
     await triggerWebhook({ email, webhookUrl, webhookSecret });
   },
